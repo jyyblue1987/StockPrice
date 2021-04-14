2c8cac94930f0ba11d93b8b6a62693964f652d17
jyyblue <jyyblue1987@outlook.com>
2021-04-14 10:16:53 +0800

    [add] get price and add data.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import threading
 import time
 import urllib.request as request
+from pymongo import MongoClient
+import json
 
 print("Start Program")
+
+client = MongoClient('localhost', 27017)
+db = client['stockprice']
+price = db['price']
 
 API_KEY = "7776525aa8b50c2410340e381ee02182"
 def fetch_data_thread(stop):
@@ -12,9 +18,26 @@ def fetch_data_thread(stop):
         print("fetch data")
         url = f"https://api.nomics.com/v1/currencies/ticker?key={API_KEY}&ids=BTC&interval=1d&convert=USD&per-page=100&page=1"
         bdata = request.urlopen(url).read()
-        data = bdata.decode('ascii')
+        res = bdata.decode('ascii')
+        list = json.loads(res)
+        if len(list) < 1:
+            time.sleep(20)
+            continue
+
+        data = list[0]
         print("Data", data)
-        time.sleep(5)
+
+        data['convert'] = 'USD'
+
+
+        # find price data
+        old = price.find_one({"id": data['id'], "convert": data["convert"], "price_timestamp": data["price_timestamp"]})
+        if old is None:
+            price_id = price.insert_one(data)
+            print("Price Data is inserted successfully. ID: ", price_id)
+
+        time.sleep(20)
+
 
 
     print("fetch_data_thread end")
@@ -25,7 +48,11 @@ if __name__ == "__main__":
     x = threading.Thread(target=fetch_data_thread, args=(lambda: stop_threads,))
     x.start()
 
-    stop_threads = True
+    while True:
+        c = input("Do you want to stop Program? (Y/N) ")
+        if c == 'Y' or c == 'y':
+            stop_threads = True
+            break
 
     x.join()
 
